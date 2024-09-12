8207f5a2827fe0c2a20da56d6a369281f080706c
NeOpa4a <nikita.boy4enko@gmail.com>
2024-09-12 11:26:55 +0300

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: path.resolve(__dirname, './src/index.js')
+    main: path.resolve(__dirname, "./src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Index',
-      template: path.resolve(__dirname, './src/pages/index.html'),
-      filename: 'index.html'
+      title: "Index",
+      template: path.resolve(__dirname, "./src/pages/index.html"),
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-      title: 'About me',
-      template: path.resolve(__dirname, './src/pages/about.html'),
-      filename: 'about.html'
+      title: "Index",
+      template: path.resolve(__dirname, "./src/pages/news.html"),
+      filename: "news.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Index",
+      template: path.resolve(__dirname, "./src/pages/photo.html"),
+      filename: "photo.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Index",
+      template: path.resolve(__dirname, "./src/pages/rozklad.html"),
+      filename: "rozklad.html",
     }),
     new CopyPlugin({
-      patterns: [{ from: './src/assets', to: 'assets' }]
-    })
+      patterns: [{ from: "./src/assets", to: "assets" }],
+    }),
   ],
   devServer: {
-    port: 8080
-  }
+    port: 8080,
+  },
 };
